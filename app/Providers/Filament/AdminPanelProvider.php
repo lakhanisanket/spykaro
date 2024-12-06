d568f19990ebe3adae5f5dd274369a6c77d46edc
lakhanisanket <sldev1998@gmail.com>
2024-12-06 19:25:58 +0530

    06-12-2024 Update

---
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Providers\Filament;
+
+use App\Models\User;
+use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\MenuItem;
+use Filament\Pages;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Widgets;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Joaopaulolndev\FilamentEditProfile\FilamentEditProfilePlugin;
+use Joaopaulolndev\FilamentEditProfile\Pages\EditProfilePage;
+use Stephenjude\FilamentBlog\BlogPlugin;
+use TomatoPHP\FilamentUsers\FilamentUsersPlugin;
+
+class AdminPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->default()
+            ->id('admin')
+            ->path('admin')
+            ->login()
+            ->brandLogo("/img/logo/final-spykaro-logo.png")
+            ->darkModeBrandLogo("/img/logo/final-spykaro-logo-white.png")
+            ->brandName('Spykaro Admin')
+            ->colors([
+                'primary' => Color::Amber,
+            ])
+            ->favicon("/img/logo/final-spykaro-logo-black-icon.png")
+            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
+            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            ->pages([
+                Pages\Dashboard::class,
+            ])
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            ->widgets([
+                Widgets\AccountWidget::class,
+                Widgets\FilamentInfoWidget::class,
+            ])
+            ->userMenuItems([
+                'profile' => MenuItem::make()
+                    ->url(fn (): string => EditProfilePage::getUrl())
+                    ->icon('heroicon-m-user-circle'),
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->plugins([
+                FilamentEditProfilePlugin::make()
+                    ->shouldRegisterNavigation(false),
+                BlogPlugin::make(),
+                FilamentShieldPlugin::make(),
+                FilamentUsersPlugin::make(),
+            ])
+            ->authMiddleware([
+                Authenticate::class,
+            ]);
+    }
+}
